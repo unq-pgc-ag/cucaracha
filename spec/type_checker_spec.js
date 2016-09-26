df8672d97e01075d1a31fa7578dcfa02312a26f9
Nahuel Garbezza <n.garbezza@gmail.com>
2016-09-26 07:23:36 -0300

    más sobre chequeo estático

--- spec/type_checker_spec.js
+++ spec/type_checker_spec.js
@@ -12,6 +12,8 @@ describe('Cucaracha - Chequeo estático', function () {
   var elSiete = { node: 'ExprConstNum', value: '7' };
   var elOcho = { node: 'ExprConstNum', value: '8' };
 
+  var expresionInvalida = { node: 'ExprAdd', expr1: elOcho, expr2: elTrue };
+
   describe('Programa', function () {
     it('es correcto cuando hay una funcion main() de tipo Unit', function () {
       var main = { node: 'Function', id: 'main', tipo: 'Unit', params: [], block: emptyBlock };
@@ -25,7 +27,7 @@ describe('Cucaracha - Chequeo estático', function () {
     });
 
     it('no es correcto cuando hay main() pero su tipo de retorno no es Unit', function () {
-      var main = { node: 'Function', id: 'main', tipo: 'Num', params: [], block: emptyBlock };
+      var main = { node: 'Function', id: 'main', tipo: 'Int', params: [], block: emptyBlock };
       var ast = { node: 'Program', functions: [main] };
       expect(chequear(ast)).toBe(false);
     });
@@ -62,12 +64,12 @@ describe('Cucaracha - Chequeo estático', function () {
     });
 
     operadoresNumericos.forEach(function (op) {
-      it(op + ' no es valida si alguno de sus argumentos no es de tipo Num', function () {
+      it(op + ' no es valida si alguno de sus argumentos no es de tipo Int', function () {
         var ast = { node: op, expr1: elSiete, expr2: elTrue };
         expect(chequear(ast)).toBe(false);
       });
 
-      it(op + ' es valida si sus dos argumentos son de tipo Num', function () {
+      it(op + ' es valida si sus dos argumentos son de tipo Int', function () {
         var ast = { node: op, expr1: elSiete, expr2: elOcho };
         expect(chequear(ast)).toBe(true);
       });
@@ -75,21 +77,122 @@ describe('Cucaracha - Chequeo estático', function () {
   });
 
   describe('llamado a procedimiento', function () {
+    var miFuncion = { node: 'StmtCall', id: 'miFuncion', expressions: [] };
+
     it('es valido cuando la funcion existe, y retorna Unit', function () {
-      var ast = { node: 'StmtCall', id: 'miFuncion', expressions: [] };
-      var context = { locals: {}, functions: { 'miFuncion' : 'Unit' }};
-      expect(chequear(ast, context)).toBe(true);
+      var context = { locals: {}, functions: { 'miFuncion' : { node: 'Function', tipo: 'Unit' } }};
+      expect(chequear(miFuncion, context)).toBe(true);
     });
 
     it('no es valido cuando la funcion no existe', function () {
-      var ast = { node: 'StmtCall', id: 'miFuncion', expressions: [] };
-      expect(chequear(ast)).toBe(false);
+      expect(chequear(miFuncion)).toBe(false);
     });
 
     it('no es valido cuando la funcion existe pero no devuelve Unit', function () {
-      var ast = { node: 'StmtCall', id: 'miFuncion', expressions: [] };
-      var context = { locals: {}, functions: { 'miFuncion' : 'Bool' }};
-      expect(chequear(ast, context)).toBe(false);
+      var context = { locals: {}, functions: { 'miFuncion' : { node: 'Function', tipo: 'Bool' } }};
+      expect(chequear(miFuncion, context)).toBe(false);
+    });
+  });
+
+  describe('return', function () {
+    it('no es válido cuando su expresión no es válida', function () {
+      var ret = { node: 'StmtReturn', expr: expresionInvalida };
+      expect(chequear(ret)).toBe(false);
+    });
+
+    it('no es valido cuando el tipo de la expresión no coincide con el tipo de retorno de la función', function () {
+      var context = { locals: {}, tipoFuncionActual: 'Bool' };
+      var ret = { node: 'StmtReturn', expr: elOcho };
+      expect(chequear(ret, context)).toBe(false);
+    });
+
+    it('es valido cuando el tipo de la expresión coincide con el tipo de retorno de la función', function () {
+      var context = { locals: {}, tipoFuncionActual: 'Int' };
+      var ret = { node: 'StmtReturn', expr: elOcho };
+      expect(chequear(ret, context)).toBe(true);
+    });
+  });
+
+  describe('armado de vector', function () {
+    it('no es valida si alguna de sus expresiones no devuelve Int', function () {
+      var vecMake = { node: 'ExprVecMake', exprs: [elOcho, elTrue] };
+      expect(chequear(vecMake)).toBe(false);
+    });
+
+    it('es valida si todas sus expresiones devuelven Int', function () {
+      var vecMake = { node: 'ExprVecMake', exprs: [elOcho, elSiete] };
+      expect(chequear(vecMake)).toBe(true);
+    });
+  });
+
+  describe('longitud de un vector', function () {
+    it('no es valida si la variable no esta definida', function () {
+      var vecLength = { node: 'ExprVecLength', id: 'x' };
+      expect(chequear(vecLength)).toBe(false);
+    });
+
+    it('no es valida si la variable esta definida pero no es de tipo Vec', function () {
+      var vecLength = { node: 'ExprVecLength', id: 'x' };
+      var context = { locals: { 'x' : 'Bool' } };
+      expect(chequear(vecLength, context)).toBe(false);
+    });
+
+    it('es valida si la variable es de tipo Vec', function () {
+      var vecLength = { node: 'ExprVecLength', id: 'x' };
+      var context = { locals: { 'x' : 'Vec' } };
+      expect(chequear(vecLength, context)).toBe(true);
+    });
+  });
+
+  describe('acceso a un vector', function () {
+    it('no es valida si el indice no es de tipo Int', function () {
+      var vecDeref = { node: 'ExprVecDeref', id: 'x', expr: elTrue };
+      var context = { locals: { 'x' : 'Vec' } };
+      expect(chequear(vecDeref, context)).toBe(false);
+    });
+
+    it('no es valida si el vector no es de tipo Vec', function () {
+      var vecDeref = { node: 'ExprVecDeref', id: 'x', expr: elOcho };
+      var context = { locals: { 'x' : 'Int' } };
+      expect(chequear(vecDeref, context)).toBe(false);
+    });
+
+    it('es valida si el indice es de tipo Int, y el vector de tipo Vec', function () {
+      var vecDeref = { node: 'ExprVecDeref', id: 'x', expr: elOcho };
+      var context = { locals: { 'x' : 'Vec' } };
+      expect(chequear(vecDeref, context)).toBe(true);
+    });
+  });
+
+  describe('asignación a vector', function () {
+    it('no es valida si la variable no es de tipo Vec', function () {
+      var vecAssign = { node: 'StmtVecAssign', id: 'x', expr1: elSiete, expr2: elOcho };
+      var context = { locals: { 'x' : 'Bool' } };
+      expect(chequear(vecAssign, context)).toBe(false);
+    });
+
+    it('no es valida si el indice no es de tipo Int', function () {
+      var vecAssign = { node: 'StmtVecAssign', id: 'x', expr1: elTrue, expr2: elOcho };
+      var context = { locals: { 'x' : 'Vec' } };
+      expect(chequear(vecAssign, context)).toBe(false);
+    });
+
+    it('no es valida si la expresión no es de tipo Int', function () {
+      var vecAssign = { node: 'StmtVecAssign', id: 'x', expr1: elSiete, expr2: elFalse };
+      var context = { locals: { 'x' : 'Vec' } };
+      expect(chequear(vecAssign, context)).toBe(false);
+    });
+
+    it('es valida si cumple los tres requisitos', function () {
+      var vecAssign = { node: 'StmtVecAssign', id: 'x', expr1: elSiete, expr2: elOcho };
+      var context = { locals: { 'x' : 'Vec' } };
+      expect(chequear(vecAssign, context)).toBe(true);
+    });
+  });
+
+  describe('if', function () {
+    it('no es valido si la condicion no es booleana', function () {
+      var elIf = { node: 'StmtIf', }
     });
   });
 
@@ -100,16 +203,16 @@ describe('Cucaracha - Chequeo estático', function () {
       });
     });
 
-    describe('llamado a funcion', function () {
-      it('retorna el tipo de retorno de la funcion que ya esta en la tabla de funciones', function () {
-        var ast = { node: 'ExprCall', id: 'miFuncion' };
-        var context = { locals: {}, functions: { 'miFuncion' : 'Bool' }};
-        expect(TypeChecker.determineTypeOf(ast, context)).toBe('Bool');
+    describe('llamado a función', function () {
+      var miFuncion = { node: 'ExprCall', id: 'miFuncion' };
+
+      it('retorna el tipo de retorno de la función que ya está en la tabla de funciones', function () {
+        var context = { locals: {}, functions: { 'miFuncion' : { node: 'Function', tipo: 'Bool' } } };
+        expect(TypeChecker.determineTypeOf(miFuncion, context)).toBe('Bool');
       });
 
-      it('retorna undefined si la funcion no está en la tabla de funciones', function () {
-        var ast = { node: 'ExprCall', id: 'miFuncion' };
-        expect(TypeChecker.determineTypeOf(ast, {})).toBe(undefined);
+      it('retorna undefined si la función no está en la tabla de funciones', function () {
+        expect(TypeChecker.determineTypeOf(miFuncion, {})).toBe(undefined);
       });
     })
   });
