6409ab128fc5d7cb7ccb86d2ab1b3e421333aefe
Nahuel Garbezza <n.garbezza@gmail.com>
2016-11-12 07:37:17 -0300

    compilar booleanos

--- spec/asm_compiler_spec.js
+++ spec/asm_compiler_spec.js
@@ -3,11 +3,25 @@ var astBuilder = require('../lib/ast_builder');
 describe('Cucaracha - Compilador Assembler', function () {
 
   describe('Constantes numéricas', function () {
-    it('escribe un ', function () {
+    it('compila un número como un mov', function () {
       var ast = astBuilder.num(8);
       var resultado = compiler.compile(ast);
 
-      expect(resultado).toBe('mov rdi, 8');
+      expect(resultado).toEqual([compiler.mov('rdi', '8')]);
+    });
+
+    it('compila un true como un mov con valor -1', function () {
+      var ast = astBuilder.true();
+      var resultado = compiler.compile(ast);
+
+      expect(resultado).toEqual([compiler.mov('rdi', '-1')]);
+    });
+
+    it('compila un false como un mov con valor 0', function () {
+      var ast = astBuilder.false();
+      var resultado = compiler.compile(ast);
+
+      expect(resultado).toEqual([compiler.mov('rdi', '0')]);
     });
   });
 });
