c4d57b6da6277a38ec581819b4eda3a21460c71e
Nahuel Garbezza <n.garbezza@gmail.com>
2016-11-21 05:25:10 -0300

    compilar llamados a funciones sin parámetros

--- spec/asm_compiler_spec.js
+++ spec/asm_compiler_spec.js
@@ -1,3 +1,5 @@
+require('jasmine-collection-matchers');
+
 var compiler = require('../lib/asm_compiler');
 var astBuilder = require('../lib/ast_builder');
 var asmBuilder = require('../lib/asm_builder');
@@ -9,21 +11,21 @@ describe('Cucaracha - Compilador Assembler', function () {
       var ast = astBuilder.num(8);
       var resultado = compiler.compile(ast);
 
-      expect(resultado).toEqual([asmBuilder.mov('rdi', '8')]);
+      expect(resultado).toHaveSameItems([asmBuilder.mov('rdi', '8')]);
     });
 
     it('compila un true como un mov con valor -1', function () {
       var ast = astBuilder.true();
       var resultado = compiler.compile(ast);
 
-      expect(resultado).toEqual([asmBuilder.mov('rdi', '-1')]);
+      expect(resultado).toHaveSameItems([asmBuilder.mov('rdi', '-1')]);
     });
 
     it('compila un false como un mov con valor 0', function () {
       var ast = astBuilder.false();
       var resultado = compiler.compile(ast);
 
-      expect(resultado).toEqual([asmBuilder.mov('rdi', '0')]);
+      expect(resultado).toHaveSameItems([asmBuilder.mov('rdi', '0')]);
     });
   });
 
@@ -32,13 +34,16 @@ describe('Cucaracha - Compilador Assembler', function () {
       var ast = astBuilder.emptyProgram();
       var resultado = compiler.compile(ast);
 
-      expect(resultado).toEqual([
+      expect(resultado).toHaveSameItems([
         asmBuilder.section('.data'),
         asmBuilder.database('lli_format_string', '"%lli"'),
         asmBuilder.section('.text'),
         asmBuilder.global('main'),
         asmBuilder.extern(['exit', 'putchar']),
         asmBuilder.subroutine('cuca_main', [
+          asmBuilder.push('rbp'),
+          asmBuilder.mov('rbp', 'rsp'),
+          asmBuilder.pop('rbp'),
           asmBuilder.ret(),
         ]),
         asmBuilder.subroutine('main', [
@@ -55,7 +60,7 @@ describe('Cucaracha - Compilador Assembler', function () {
       var ast = astBuilder.putChar(astBuilder.num('72'));
       var resultado = compiler.compile(ast);
 
-      expect(resultado).toEqual([
+      expect(resultado).toHaveSameItems([
         asmBuilder.mov('rdi', '72'),
         asmBuilder.call('putchar'),
       ]);
@@ -67,7 +72,7 @@ describe('Cucaracha - Compilador Assembler', function () {
       var ast = astBuilder.putNum(astBuilder.num('123'));
       var resultado = compiler.compile(ast);
 
-      expect(resultado).toEqual([
+      expect(resultado).toHaveSameItems([
         asmBuilder.mov('rsi', '123'),
         asmBuilder.mov('rdi', 'lli_format_string'),
         asmBuilder.mov('rax', 0),
@@ -77,23 +82,22 @@ describe('Cucaracha - Compilador Assembler', function () {
   });
 
   describe('Definición de funciones', function () {
-    it('compila con nombre cuca_nombre, apila parámetros y luego los  desapila al final', function () {
+    it('compila con nombre cuca_nombre', function () {
       var ast = astBuilder.emptyUnitFunction('miFuncion', []);
       var resultado = compiler.compile(ast);
 
-      expect(resultado).toEqual([
+      expect(resultado).toHaveSameItems([
         asmBuilder.subroutine('cuca_miFuncion', [
           asmBuilder.push('rbp'),
           asmBuilder.mov('rbp', 'rsp'),
-          asmBuilder.sub('rsp', '0'),
-          asmBuilder.mov('rbp', 'rsp'),
+          // cuerpo de la función
           asmBuilder.pop('rbp'),
           asmBuilder.ret(),
         ])
       ]);
     });
 
-    it('con x cantidad de variables locales, actualiza rsp con esa cantidad', function () {
+    it('con x cantidad de variables locales, actualiza rsp con esa cantidad y lo restaura al final', function () {
       var block = astBuilder.block([
         astBuilder.assign('x', astBuilder.num('42')),
         astBuilder.assign('y', astBuilder.true()),
@@ -101,7 +105,7 @@ describe('Cucaracha - Compilador Assembler', function () {
       var ast = astBuilder.unitFunction('miFuncion', [], block);
       var resultado = compiler.compile(ast);
 
-      expect(resultado).toEqual([
+      expect(resultado).toHaveSameItems([
         asmBuilder.subroutine('cuca_miFuncion', [
           asmBuilder.push('rbp'),
           asmBuilder.mov('rbp', 'rsp'),
@@ -112,5 +116,43 @@ describe('Cucaracha - Compilador Assembler', function () {
         ])
       ]);
     });
+  });
+
+  describe('Llamado a funciones', function () {
+    it('compila un llamado a una funcion sin parámetros', function () {
+      var miFuncion = astBuilder.emptyUnitFunction('miFuncion', []);
+      var mainBlock = astBuilder.block([astBuilder.funcCall('miFuncion')]);
+      var ast = astBuilder.programWith(miFuncion, mainBlock);
+      var resultado = compiler.compile(ast);
+
+      expect(resultado).toHaveSameItems([
+        asmBuilder.section('.data'),
+        asmBuilder.database('lli_format_string', '"%lli"'),
+        asmBuilder.section('.text'),
+        asmBuilder.global('main'),
+        asmBuilder.extern(['exit', 'putchar']),
+        // TODO preguntar: hace falta que estén ordenadas las definiciones de funciones de manera que el main quede al final?
+        asmBuilder.subroutine('cuca_main', [
+          asmBuilder.push('rbp'),
+          asmBuilder.mov('rbp', 'rsp'),
+          asmBuilder.call('cuca_miFuncion'),
+          asmBuilder.pop('rbp'),
+          asmBuilder.ret(),
+        ]),
+        asmBuilder.subroutine('cuca_miFuncion', [
+          asmBuilder.push('rbp'),
+          asmBuilder.mov('rbp', 'rsp'),
+          asmBuilder.pop('rbp'),
+          asmBuilder.ret(),
+        ]),
+        asmBuilder.subroutine('main', [
+          asmBuilder.call('cuca_main'),
+          asmBuilder.mov('rdi', '0'),
+          asmBuilder.call('exit'),
+        ]),
+      ]);
+    });
+
+    xit('compila un llamado a una función con parámetros');
   });
 });
