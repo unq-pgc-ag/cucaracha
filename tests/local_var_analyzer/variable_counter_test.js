715d0ff6da3d1258ed255ed13852a5beb603a264
Nahuel Garbezza <n.garbezza@gmail.com>
2020-08-02 17:32:32 -0300

    :recycle: simple local variable refactorings

--- tests/local_var_analyzer/variable_counter_test.js
+++ tests/local_var_analyzer/variable_counter_test.js
@@ -3,26 +3,24 @@ const { suite, test, assert } = require('@pmoo/testy');
 const astBuilder = require('../../lib/ast_builder');
 const localVarAnalyzer = require('../../lib/local_var_analyzer');
 
+const numberAssignment = astBuilder.assign('x', astBuilder.num('42'));
+const booleanAssignment = astBuilder.assign('y', astBuilder.true());
+
 suite('Cucaracha - Análisis de Variables Locales - Contador', () => {
   test('cuenta una asignación de variable', () => {
-    const ast = astBuilder.assign('x', astBuilder.num('42'));
-    const resultado = localVarAnalyzer.count(ast);
+    const resultado = localVarAnalyzer.count(numberAssignment);
 
     assert.that(resultado).isEqualTo(1);
   });
 
   test('no cuenta una asignación de variable si la variable ya fue vista', () => {
-    const ast = astBuilder.assign('x', astBuilder.num('42'));
-    const resultado = localVarAnalyzer.count(ast, { x : 2 });
+    const resultado = localVarAnalyzer.count(numberAssignment, { x : 2 });
 
     assert.that(resultado).isEqualTo(1);
   });
 
   test('cuenta un bloque con muchas asignaciones de variables', () => {
-    const ast = astBuilder.block([
-      astBuilder.assign('x', astBuilder.num('42')),
-      astBuilder.assign('y', astBuilder.true()),
-    ]);
+    const ast = astBuilder.block([numberAssignment, booleanAssignment]);
     const resultado = localVarAnalyzer.count(ast);
 
     assert.that(resultado).isEqualTo(2);
--- tests/local_var_analyzer/variable_list_test.js
+++ tests/local_var_analyzer/variable_list_test.js
@@ -3,26 +3,24 @@ const { suite, test, assert } = require('@pmoo/testy');
 const astBuilder = require('../../lib/ast_builder');
 const localVarAnalyzer = require('../../lib/local_var_analyzer');
 
+const numberAssignment = astBuilder.assign('x', astBuilder.num('42'));
+const booleanAssignment = astBuilder.assign('y', astBuilder.true());
+
 suite('Cucaracha - Análisis de Variables Locales - Listado', () => {
   test('lista una asignación de variable', () => {
-    const ast = astBuilder.assign('x', astBuilder.num('42'));
-    const resultado = localVarAnalyzer.allVarNames(ast);
+    const resultado = localVarAnalyzer.allVarNames(numberAssignment);
 
     assert.that(resultado).includesExactly('x');
   });
 
   test('lista sólo una vez una asignación', () => {
-    const ast = astBuilder.assign('x', astBuilder.num('42'));
-    const resultado = localVarAnalyzer.allVarNames(ast, { x : 2 });
+    const resultado = localVarAnalyzer.allVarNames(numberAssignment, { x : 2 });
 
     assert.that(resultado).includesExactly('x');
   });
 
   test('lista varias asignaciones de variables', () => {
-    const ast = astBuilder.block([
-      astBuilder.assign('x', astBuilder.num('42')),
-      astBuilder.assign('y', astBuilder.true()),
-    ]);
+    const ast = astBuilder.block([numberAssignment, booleanAssignment]);
     const resultado = localVarAnalyzer.allVarNames(ast);
 
     assert.that(resultado).includesExactly('x', 'y');
